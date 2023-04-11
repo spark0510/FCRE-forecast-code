e9151d58c3b8253d1566be2b1d52ceddbddab69c
Austin Delany <addelany@gmail.com>
2023-04-11 10:35:18 -0500

    comment out wq test code

--- R/run_edi_data_bind.R
+++ R/run_edi_data_bind.R
@@ -1,22 +1,22 @@
 
 
-edi_file <- dt1
+#edi_file <- dt1
 
-source('R/edi_qaqc_combine.R')
-library(tidyverse)
-library(lubridate)
-library(yaml)
+#source('R/edi_qaqc_combine.R')
+#library(tidyverse)
+#library(lubridate)
+#library(yaml)
 
 # open config file
-config_file <- read_yaml('https://raw.githubusercontent.com/FLARE-forecast/FCRE-forecast-code/main/configuration/defaultV2/observation_processing.yml')
+#config_file <- read_yaml('https://raw.githubusercontent.com/FLARE-forecast/FCRE-forecast-code/main/configuration/defaultV2/observation_processing.yml')
 
-L1_file <- read.csv('https://raw.githubusercontent.com/FLARE-forecast/FCRE-data/fcre-catwalk-data/fcre-waterquality_L1.csv',
-                    na.strings = 'NA', stringsAsFactors = FALSE)
+#L1_file <- read.csv('https://raw.githubusercontent.com/FLARE-forecast/FCRE-data/fcre-catwalk-data/fcre-waterquality_L1.csv',
+#                    na.strings = 'NA', stringsAsFactors = FALSE)
 
-edi_file <- read_csv('https://portal.edirepository.org/nis/dataviewer?packageid=edi.271.7&entityid=71e6b946b751aa1b966ab5653b01077f')
+#edi_file <- read_csv('https://portal.edirepository.org/nis/dataviewer?packageid=edi.271.7&entityid=71e6b946b751aa1b966ab5653b01077f')
 
 
-test_df <- wq_realtime_edi_combine(realtime_file = 'https://raw.githubusercontent.com/FLARE-forecast/FCRE-data/fcre-catwalk-data/fcre-waterquality_L1.csv',
-                            qaqc_file = 'https://portal.edirepository.org/nis/dataviewer?packageid=edi.271.7&entityid=71e6b946b751aa1b966ab5653b01077f',
-                            config_file = 'https://raw.githubusercontent.com/FLARE-forecast/FCRE-forecast-code/main/configuration/defaultV2/observation_processing.yml',
-                            input_file_tz = "America/New_York")
+#test_df <- wq_realtime_edi_combine(realtime_file = 'https://raw.githubusercontent.com/FLARE-forecast/FCRE-data/fcre-catwalk-data/fcre-waterquality_L1.csv',
+ #                           qaqc_file = 'https://portal.edirepository.org/nis/dataviewer?packageid=edi.271.7&entityid=71e6b946b751aa1b966ab5653b01077f',
+  #                          config_file = 'https://raw.githubusercontent.com/FLARE-forecast/FCRE-forecast-code/main/configuration/defaultV2/observation_processing.yml',
+   #                         input_file_tz = "America/New_York")
